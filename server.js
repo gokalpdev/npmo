7ea771c3681742f50879092830234d81a85c4375
No-exceptions <8astard8word@gmail.com>
2020-07-04 23:08:13 +0300

    j

--- server.js
+++ server.js
@@ -61,6 +61,9 @@ app.get("/", function(req, res){
 		res.sendFile(__dirname + "/views/index.html");
 	})
 
+app.use('/public', express.static(__dirname + '/public'));
+
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
   bGround.log('Node.js listening ...');
