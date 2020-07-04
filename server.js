4500bafb3ce7d0159fef991b3de185a598a0a758
No-exceptions <8astard8word@gmail.com>
2020-07-04 23:20:41 +0300

    j

--- server.js
+++ server.js
@@ -57,12 +57,7 @@ if (!process.env.DISABLE_XORIGIN) {
   // }  
 // })
 
-app.get("/", function(req, res){
-		res.sendFile(__dirname + "/views/index.html");
-	})
 
-app.use(express.static(__dirname + "/public"));
-app.use('/public', express.static(__dirname + "/public"));
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function () {
