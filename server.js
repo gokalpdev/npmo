aa4a11ee678a7844a01284f7c267876f3a1234f9
No-exceptions <8astard8word@gmail.com>
2020-07-04 23:18:43 +0300

    j

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.get("/", function(req, res){
 		res.sendFile(__dirname + "/views/index.html");
 	})
 
-//app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 app.use('/public', express.static(__dirname + "/public"));
 
 var port = process.env.PORT || 3000;
